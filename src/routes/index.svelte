7b354b8d6f1d6113fcbc2611c5b8dc4baedf1d42
AnonJr <whyashplaysdota@gmail.com>
2022-01-28 02:17:41 +0530

    inital commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<div class="flex justify-center">
+    <div class="flex justify-center bg-black w-full">
+        <h1 class="my-5 font-semibold text-3xl text-white px-2">Starter Template</h1>
+    </div>
+</div>
---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./src/**/*.svelte', './src/**/*.css'],
+  darkMode: true,
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
