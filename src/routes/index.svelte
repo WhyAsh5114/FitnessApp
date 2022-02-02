8637bcc13123686804eb55cfea0a4f6cb1b0e561
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-02-02 17:54:05 +0530

    add title and bg

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,7 @@
-<div class="flex justify-center">
-    <div class="flex justify-center bg-black w-full">
-        <h1 class="my-5 font-semibold text-3xl text-white px-2">Starter Template</h1>
-    </div>
+<script type="typescript">
+    import Title from "./title.svelte";
+</script>
+
+<div class="bg-blue-900 h-screen">
+    <Title />
 </div>
