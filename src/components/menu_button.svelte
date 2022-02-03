46aabd4f950e4afa34ef3283c83c1bce5edc545d
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-02-04 03:15:34 +0530

    made the menu button a component

---
+++ src/components/menu_button.svelte
@@ -0,0 +1,27 @@
+<script>
+    export let color, imgPath, imgTailwind, title, content;
+    // @ts-ignore
+    let hoveredColor = color.replace("7", "8");
+    // @ts-ignore
+    let link = title.toLowerCase()
+    console.log(hoveredColor)
+</script>
+
+<a href="{link}" class="{color} rounded-3xl grid place-items-center grid-cols-3 gap-3 btn-grid shadow-md shadow-black hover:brightness-90 hover:shadow-black hover:shadow-xl">
+    <img src="{imgPath}" alt="{title}" class="{imgTailwind} p-5">
+    <div class="grid grid-rows-2 w-full h-3/4 md:h-1/2 items-center">
+        <h2 class="text-white font-semibold text-2xl md:text-3xl lg:text-4xl text-left w-full">
+            {title}
+        </h2>
+        <p class="text-white text-md md:text-xl lg:text-2xl w-full text-left pr-2">
+            {content}
+        </p>
+    </div>
+</a>
+
+<style>
+    .btn-grid
+    {
+        grid-template-columns: 1fr 2fr;
+    }
+</style>
---
+++ src/routes/app/home_grid.svelte
@@ -0,0 +1,10 @@
+<script>
+    import MenuButton from "/src/components/menu_button.svelte";
+</script>
+
+<div class="grid grid-rows-4 md:grid-cols-2 md:grid-rows-2 gap-5 h-full w-full justify-items-stretch p-5">
+    <MenuButton color="bg-pink-700" imgPath="/SplitsLogo.png" imgTailwind="w-32 md:w-44" title="Splits" content="Make weekly routines, modify current split"/>
+    <MenuButton color="bg-purple-700" imgPath="/WorkoutsLogo.png" imgTailwind="w-32 md:w-52" title="Workouts" content="Log workout, cardio, nutrition and more"/>
+    <MenuButton color="bg-cyan-700" imgPath="/RecordsLogo.png" imgTailwind="w-24 md:w-36" title="Records" content="See and modify previous workouts, log body metrics"/>
+    <MenuButton color="bg-teal-700" imgPath="/TrackingLogo.png" imgTailwind="w-32 md:w-52" title="Tracking" content="Visualise your progress using multiple metrics"/>
+</div>
