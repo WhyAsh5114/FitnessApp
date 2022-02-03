2eb6b7f46ff2ef2b055d316628a841a707f358c6
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-02-04 03:29:11 +0530

    remove unused code

--- src/components/menu_button.svelte
+++ src/components/menu_button.svelte
@@ -1,13 +1,11 @@
 <script>
     export let color, imgPath, imgTailwind, title, content;
     // @ts-ignore
-    let hoveredColor = color.replace("7", "8");
-    // @ts-ignore
     let link = title.toLowerCase()
-    console.log(hoveredColor)
 </script>
 
-<a href="{link}" class="{color} rounded-3xl grid place-items-center grid-cols-3 gap-3 btn-grid shadow-md shadow-black hover:brightness-90 hover:shadow-black hover:shadow-xl">
+<a href="{link}" class="{color} rounded-3xl grid place-items-center grid-cols-3 gap-3 shadow-md shadow-black 
+hover:brightness-90 hover:shadow-black hover:shadow-xl" style="grid-template-columns: 1fr 2fr">
     <img src="{imgPath}" alt="{title}" class="{imgTailwind} p-5">
     <div class="grid grid-rows-2 w-full h-3/4 md:h-1/2 items-center">
         <h2 class="text-white font-semibold text-2xl md:text-3xl lg:text-4xl text-left w-full">
@@ -18,10 +16,3 @@
         </p>
     </div>
 </a>
-
-<style>
-    .btn-grid
-    {
-        grid-template-columns: 1fr 2fr;
-    }
-</style>
