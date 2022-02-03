884f473dbdd39bb3826cb9afd428372397001896
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-02-04 03:51:24 +0530

    globalised button container

--- src/routes/app/home_grid.svelte
+++ src/routes/app/home_grid.svelte
@@ -2,7 +2,9 @@
     import MenuButton from "/src/components/menu_button.svelte";
 </script>
 
-<div class="grid grid-rows-4 md:grid-cols-2 md:grid-rows-2 gap-5 h-full w-full justify-items-stretch p-5">
+<link rel="stylesheet" href="/src/global.css">
+
+<div class="buttons-container">
     <MenuButton color="bg-pink-700" imgPath="/SplitsLogo.png" imgTailwind="w-32 md:w-44" title="Splits" content="Make weekly routines, modify current split"/>
     <MenuButton color="bg-purple-700" imgPath="/WorkoutsLogo.png" imgTailwind="w-32 md:w-52" title="Workouts" content="Log workout, cardio, nutrition and more"/>
     <MenuButton color="bg-cyan-700" imgPath="/RecordsLogo.png" imgTailwind="w-24 md:w-36" title="Records" content="See and modify previous workouts, log body metrics"/>
--- src/routes/splits/splits_grid.svelte
+++ src/routes/splits/splits_grid.svelte
@@ -2,7 +2,9 @@
     import MenuButton from "/src/components/menu_button.svelte";
 </script>
 
-<div class="grid grid-rows-4 md:grid-cols-2 md:grid-rows-2 gap-5 h-full w-full justify-items-stretch p-5">
+<link rel="stylesheet" href="/src/global.css">
+
+<div class="buttons-container">
     <MenuButton color="bg-pink-700" imgPath="/SplitsLogo.png" imgTailwind="w-32 md:w-44" title="New Split" content="Create a weekly routine from scratch"/>
     <MenuButton color="bg-fuchsia-700" imgPath="/WorkoutsLogo.png" imgTailwind="w-32 md:w-52" title="Common Splits" content="Adjust and use common splits"/>
     <MenuButton color="bg-purple-700" imgPath="/RecordsLogo.png" imgTailwind="w-24 md:w-36" title="Splits Records" content="View, copy and adjust previous splits"/>
