e485b32364216d1f9e9d4211f701f06036428b9d
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-02-06 11:54:46 +0530

    style changed logging icon to a pencil

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,8 +7,8 @@
 <BasePage>
     <MenuButtonGrid>
         <MenuButton color="bg-pink-700" imgPath="/calendar.svg" title="Splits" content="Make weekly routines, modify current split"/>
-        <MenuButton color="bg-purple-700" imgPath="/dumbbell.svg" title="Logging" content="Log workout, cardio, nutrition and more"/>
-        <MenuButton color="bg-cyan-700" imgPath="/record.svg" title="Records" content="See and modify previous workouts, log body metrics"/>
+        <MenuButton color="bg-purple-700" imgPath="/pencil.svg" title="Logging" content="Log workout, cardio, nutrition and more"/>
+        <MenuButton color="bg-cyan-700" imgPath="/record.svg" title="Records" content="See and modify logged stats like workout and cardio"/>
         <MenuButton color="bg-teal-700" imgPath="/graph.svg" title="Tracking" content="Visualise your progress using multiple metrics"/>
     </MenuButtonGrid>
 </BasePage>
