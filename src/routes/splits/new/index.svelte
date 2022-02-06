6375826b02f729beb34693207992efcd30e5c894
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-02-06 19:37:40 +0530

    added the create split page

---
+++ src/routes/splits/new/index.svelte
@@ -0,0 +1,55 @@
+<script type="typescript">
+    import BasePage from "/src/components/base_page.svelte";
+
+    let number_of_workouts: number = 0;
+    let days: Array<string> = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    let workouts: Array<string> = ['', '', '', '', '', '', ''];
+
+    function update_value() {
+        let unique_workouts: Array<String> = [];
+        workouts.forEach((workout, i) => {
+            workouts[i] = workouts[i].charAt(0).toUpperCase() + workouts[i].slice(1);
+            workout = workout.toLowerCase();
+            if(workout === "rest") {
+                workouts[i] = "";
+            }
+            if(!unique_workouts.includes(workout) && workout !== "" && workout !== "rest") {
+                unique_workouts.push(workout);
+            }
+        });
+        number_of_workouts = unique_workouts.length;
+    }
+</script>
+
+
+<style>
+    .new-split {
+        grid-template-rows: 1fr 1fr 5fr 1fr;
+    }
+</style>
+
+
+<BasePage>
+    <div class="grid w-5/6 md:w-4/6 lg:w-3/6 xl:w-2/6 gap-3 px-5 h-full new-split place-items-center">
+        <h2 class="text-2xl text-white font-bold text-center">
+            Create New Split
+        </h2>
+        <div class="grid grid-cols-2 rounded-md">
+            <h3 class="text-xl text-white text-center font-semibold">Name</h3>
+            <input type="text" class="text-center outline-none rounded-sm font-semibold shadow-black shadow-sm mr-5">
+        </div>
+        <div class="grid gap-2">
+            {#each days as day, i}
+                <div class="grid grid-cols-2 shadow-black bg-pink-700 rounded-full p-2 w-full">
+                    <p class="text-white text-center text-lg font-medium">
+                        {day}
+                    </p>
+                    <input bind:value={workouts[i]} on:change="{update_value}" type="text" placeholder="Rest" class="text-center mr-5 outline-none p-0.5 rounded-sm font-medium shadow-black shadow-sm">
+                </div>
+            {/each}
+        </div>
+        <a class="text-white font-semibold text-lg py-1 border-pink-600 border-2 rounded-full px-4" href="workouts">
+            Create {number_of_workouts} workouts
+        </a>
+    </div>
+</BasePage>
