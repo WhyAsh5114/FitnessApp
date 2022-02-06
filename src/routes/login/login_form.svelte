598e4c530b78966574d6d47b98223382df43b745
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-02-06 11:55:37 +0530

    style fix fixed login form height

--- src/routes/login/login_form.svelte
+++ src/routes/login/login_form.svelte
@@ -1,4 +1,4 @@
-<div class="grid bg-stone-200 m-2 px-6 pt-4 pb-2 md:w-96 rounded-md justify-items-center">
+<div class="grid bg-stone-200 m-2 px-6 pt-4 pb-2 md:w-96 rounded-md justify-items-center h-fit">
     <h1 class="text-2xl text-slate-900 font-semibold mb-1">Welcome</h1>
     <h1 class="text-lg text-slate-900 mb-4">Login to continue</h1>
     <input placeholder="Username" type="text" class="border-gray-400 border-2 p-2 h-10 outline-none -my-px w-full focus:z-10 focus:border-black">
