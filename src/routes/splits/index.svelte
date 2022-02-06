08e62bbf75a79a361dfb4281ef5a8536b168bb96
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-02-06 13:34:38 +0530

    added splits menu icons

--- src/routes/splits/index.svelte
+++ src/routes/splits/index.svelte
@@ -6,9 +6,9 @@
 
 <BasePage>
     <MenuButtonGrid>
-        <MenuButton color="bg-pink-700" imgPath="/SplitsLogo.png" title="New Split" content="Create a weekly routine from scratch" link="splits/new"/>
-        <MenuButton color="bg-fuchsia-700" imgPath="/WorkoutsLogo.png" title="Common Splits" content="Adjust and use common splits" link="splits/common"/>
-        <MenuButton color="bg-purple-700" imgPath="/WorkoutsLogo.png" title="Modify Split" content="Modify current split's exercises, progression" link="splits/modify"/>
-        <MenuButton color="bg-violet-700" imgPath="/RecordsLogo.png" title="Splits Records" content="View, copy and adjust previous splits" link="records/splits"/>
+        <MenuButton color="bg-pink-700" imgPath="/calendar_plus.svg" title="New Split" content="Create a weekly routine from scratch" link="splits/new"/>
+        <MenuButton color="bg-fuchsia-700" imgPath="/calendar_PPL.svg" title="Common Splits" content="Adjust and use common splits" link="splits/common"/>
+        <MenuButton color="bg-purple-700" imgPath="/calendar_gear.svg" title="Modify Split" content="Modify current split's exercises, progression" link="splits/modify"/>
+        <MenuButton color="bg-violet-700" imgPath="/record.svg" title="Splits Records" content="View, copy and adjust previous splits" link="records/splits"/>
     </MenuButtonGrid>
 </BasePage>
