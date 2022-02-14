f3ed3daf13c5919c929e22cc0ee9cbdbe243a0b0
WhyAsh5114 <whyash5114@gmail.com>
2022-02-15 01:08:17 +0530

    setup prerequisites for the modal

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,27 @@
 <script>
     import '../tailwind.css'
+    import { Modals, closeModal } from 'svelte-modals'
 </script>
+
+<Modals>
+    <div
+        slot="backdrop"
+        class="backdrop"
+        on:click={closeModal}
+    />
+</Modals>
   
 <div class="">
     <slot></slot>
 </div>
+
+<style>
+    .backdrop {
+        position: fixed;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        left: 0;
+        background: rgba(0,0,0,0.50)
+    }
+</style>
