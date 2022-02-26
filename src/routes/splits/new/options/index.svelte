6f1aa96fb288fddaf191687d9074e4fdf4ca5441
WhyAsh5114 <whyash5114@gmail.com>
2022-02-26 23:44:44 +0530

    added slider options

--- src/routes/splits/new/options/index.svelte
+++ src/routes/splits/new/options/index.svelte
@@ -1,17 +1,40 @@
 <script type="typescript">
+	import { to_number } from 'svelte/internal';
+
 	import { SplitWorkouts } from '../newSplitStore';
 
 	let split_workouts: Object;
 	SplitWorkouts.subscribe((value: Object) => {
 		split_workouts = value;
 	});
+
+	let value = 5;
 </script>
 
-<div class="w-full py-4 px-10 grid place-items-center">
-	<div class="slidecontainer w-5/6">
-		<input type="range" min="0" max="50" value="5" class="slider"/>
-		<p class="text-white">Value: </p>
+<div class="grid gap-10 w-full place-items-center">
+	<h1 class="text-white text-center text-3xl font-semibold">Split Progression</h1>
+	<div class="w-full py-4 px-10 grid place-items-center lg:grid-cols-2">
+		<div class="slidecontainer w-full p-5">
+			<input type="range" min="0" max="25" class="slider" bind:value />
+		</div>
+		<div class="flex flex-row w-full justify-center gap-3">
+			<p class="text-white text-center bg-slate-900 px-4 py-1">
+				{value}
+			</p>
+			<select class="outline-none bg-slate-900 px-2 py-1 text-white">
+				<option value="">%</option>
+				<option value="">lb</option>
+				<option value="">kg</option>
+			</select>
+			<p class="text-white text-xl font-bold">/</p>
+			<select class="outline-none bg-slate-900 px-2 py-1 text-white">
+				<option value="">session</option>
+				<option value="">week</option>
+				<option value="">month</option>
+			</select>
+		</div>
 	</div>
+	<button class="text-white font-semibold rounded-full bg-blue-600 w-1/2 py-1">Save Split</button>
 </div>
 
 <style>
@@ -22,7 +45,7 @@
 		outline: none;
 		-webkit-transition: 0.2s;
 		transition: opacity 0.2s;
-        @apply bg-slate-500 rounded-sm;
+		@apply bg-slate-500 rounded-sm;
 	}
 
 	.slider::-webkit-slider-thumb {
@@ -32,7 +55,7 @@
 		height: 25px;
 		border-radius: 50%;
 		cursor: pointer;
-        @apply bg-teal-400;
+		@apply bg-teal-400;
 	}
 
 	.slider::-moz-range-thumb {
@@ -40,6 +63,6 @@
 		height: 25px;
 		border-radius: 50%;
 		cursor: pointer;
-        @apply bg-teal-400;
+		@apply bg-teal-400;
 	}
 </style>
