7857fbe0af9650d222393d79449936e3a12b232b
WhyAsh5114 <whyash5114@gmail.com>
2022-04-17 00:06:33 +0530

    designed basic profile page

---
+++ src/routes/profile/index.svelte
@@ -0,0 +1,40 @@
+<script context="module" lang="ts">
+	export async function load({ session }) {
+		// If user not logged in, redirect to login
+		if (!session?.user) {
+			return {
+				status: 302,
+				redirect: '/profile/login'
+			};
+		// Else set username to be accessible in client
+		} else {
+			return {
+				props: {
+					username: session.user.username
+				}
+			};
+		}
+	}
+</script>
+
+<script lang="ts">
+	export let username;
+
+	async function logout() {
+		await fetch('/api/logout', {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+        window.location.href = '/profile/login'
+	}
+</script>
+
+<div class="flex flex-col place-items-center">
+	<p class="text-white w-full text-center mb-10">Hi {username}</p>
+	<button
+		class="text-white text-md bg-red-500 px-3 py-1 rounded-full hover:bg-red-600"
+		on:click={logout}>Logout</button
+	>
+</div>
