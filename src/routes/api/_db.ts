2af0df60e547b1e0ddc98bcd1be04735a040c563
WhyAsh5114 <whyash5114@gmail.com>
2022-04-17 00:04:58 +0530

    implemented database API

---
+++ src/routes/api/_db.ts
@@ -0,0 +1,66 @@
+import { createClient } from "redis";
+import { v4 as uuid } from 'uuid';
+import { compare, hash } from 'bcrypt';
+
+const db = createClient();
+db.connect()
+
+export const getUser = async (username: string): Promise<unknown> => {
+    const existingUser = await db.get(username);
+    if (!existingUser) {
+        return Promise.resolve(null);
+    } else {
+        return Promise.resolve(existingUser);
+    }
+}
+
+export const registerUser = async (user: { username: string, password: string }): Promise<unknown> => {
+    const existingUser = await db.get(user.username);
+    if (existingUser) {
+        return Promise.reject(new Error('User already exists'));
+    } else {
+        const hashedPassword = await hash(user.password, 10);
+        user.password = hashedPassword;
+        await db.set(user.username, JSON.stringify(user));
+        return Promise.resolve(user);
+    }
+}
+
+export const loginUser = async(user: { username: string, password: string }): Promise<string> => {
+    const existingUser = JSON.parse(await db.get(user.username));
+    if (!existingUser) {
+        return Promise.reject(new Error('User does not exist'));
+    } else {
+        if (!await compare(user.password, existingUser.password)) {
+            return Promise.reject(new Error('Incorrect password'));
+        } else {
+            const sessionID = await createSession(user.username);
+            return Promise.resolve(sessionID);
+        }
+    }
+}
+
+export const createSession = async (username: string): Promise<string> => {
+    const sessionID = uuid();
+    await db.set(sessionID, username);
+    return Promise.resolve(sessionID);
+}
+
+export const getSession = async (sessionID: string): Promise<string> => {
+    const username = await db.get(sessionID);
+    if (!username) {
+        return Promise.resolve(null);
+    } else {
+        return Promise.resolve(username);
+    }
+}
+
+export const removeSession = async (sessionID: string): Promise<unknown> => {
+    const username = await db.get(sessionID);
+    if (!username) {
+        return Promise.reject(new Error("Session not found"))
+    } else {
+        await db.del(sessionID);
+        return Promise.resolve(sessionID);
+    }
+}
