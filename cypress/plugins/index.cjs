15ef49e4d9d983fba6bc230e026b14e0a80dd9fc
WhyAsh5114 <whyash5114@gmail.com>
2022-04-24 00:19:45 +0530

    added a flush db task in cypress this will allow to consistently test API endpoints in the login process

--- cypress/plugins/index.cjs
+++ cypress/plugins/index.cjs
@@ -1,4 +1,7 @@
 /// <reference types="cypress" />
+
+const { createClient } = require("redis");
+
 // ***********************************************************
 // This example plugins/index.js can be used to load plugins
 //
@@ -15,8 +18,17 @@
 /**
  * @type {Cypress.PluginConfig}
  */
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+	// `on` is used to hook into various events Cypress emits
+	// `config` is the resolved Cypress config
+
+	on('task', {
+		async flushDB() {
+			const db = createClient();
+			await db.connect();
+			await db.flushAll();
+			return null;
+		}
+	})
 }
