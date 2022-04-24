ceb1e27999df0f0a26b00f4fac47f24a2c6fe5c4
WhyAsh5114 <whyash5114@gmail.com>
2022-04-24 18:21:26 +0530

    added a login command in cypress

---
+++ cypress/support/index.ts
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+Cypress.Commands.add('login', (username: string, password: string) => {
+    cy.session([username, password], () => {
+        cy.visit('http://localhost:3000/profile/login')
+        cy.get('[placeholder=Username]').type(username)
+        cy.get('[placeholder=Password]').type(password)
+        cy.get('button').contains('Submit').click()
+        cy.url().should('equal', 'http://localhost:3000/profile')
+    })
+})
