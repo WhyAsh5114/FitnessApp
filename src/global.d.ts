72649509cbdf9e079f7d9bf6e0faf37f6d9ffb51
WhyAsh5114 <whyash5114@gmail.com>
2022-07-18 17:22:05 +0530

    added global types for user, split, etc

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,30 @@
 /// <reference types="@sveltejs/kit" />
+
+interface user {
+	username: string,
+	password: string,
+	splits: Record<string, split>,
+	workouts?: {
+
+	}
+}
+
+interface split {
+    name: string,
+    schedule: string[],
+    split_workouts: Record<string, split_workout>
+}
+
+interface split_workout {
+    name: string,
+    days: string[],
+    exercises: Record<string, exercise>
+}
+
+interface exercise {
+    id: number,
+    name: string,
+    reps: number,
+    sets: number,
+    load: number
+}
