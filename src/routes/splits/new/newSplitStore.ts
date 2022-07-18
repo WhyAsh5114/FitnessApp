784ab92f2ec84f846edf2a33e6a7b9865098053a
WhyAsh5114 <whyash5114@gmail.com>
2022-07-18 17:21:28 +0530

    added type to SplitSchedule store

--- src/routes/splits/new/newSplitStore.ts
+++ src/routes/splits/new/newSplitStore.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
 
 export const SplitName = writable('');
-export const SplitSchedule = writable([]);
+export const SplitSchedule = writable(new Array<string>);
 export const SplitWorkouts = writable({});
