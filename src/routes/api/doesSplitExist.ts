de0304697f60ca1abe232a3fb255078da6a38e48
WhyAsh5114 <whyash5114@gmail.com>
2022-07-18 18:14:41 +0530

    changed api route follows latest sveltekit conventions

---
+++ src/routes/api/doesSplitExist.ts
@@ -0,0 +1,28 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import { getUser } from './_db';
+
+export const POST: RequestHandler = async ({ request, locals }) => {
+	const body = await request.json();
+
+	if (locals.username) {
+		const user = JSON.parse(await getUser(locals.username));
+		if (!user['splits'][body.split_name]) {
+			return {
+				status: 200,
+				body: 'Split can be created'
+			};
+		} else {
+			return {
+				status: 409,
+				body: {
+					message: 'Split already exists'
+				}
+			};
+		}
+	} else {
+		return {
+			status: 401,
+			message: 'Invalid session'
+		};
+	}
+};
