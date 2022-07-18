80d301b54d0e70fce0eb366fdbaeacdccb902eba
WhyAsh5114 <whyash5114@gmail.com>
2022-07-18 18:15:02 +0530

    prettier

--- src/routes/api/saveSplit.ts
+++ src/routes/api/saveSplit.ts
@@ -1,29 +1,30 @@
-import { getUser, setUser } from "./_db"
-import type { Request } from "@sveltejs/kit"
+import type { RequestHandler } from '@sveltejs/kit';
+import { getUser, setUser } from './_db';
 
-export async function post({ body, locals }: Request & { body: string }): Promise<unknown> {
-    if (locals.user?.username) {
-        const user = JSON.parse(await getUser(locals.user.username));
-        const { name, schedule, workouts } = JSON.parse(body)
-        
-        user.splits[name] = { schedule, workouts }
+export const POST: RequestHandler = async ({ request, locals }) => {
+	if (locals.username) {
+		const user: user = JSON.parse(await getUser(locals.username));
+		const { name, schedule, workouts }: { name: string; schedule: string[]; workouts: any } =
+			await request.json();
 
-        try {
-            await setUser(locals.user.username, JSON.stringify(user));
-            return {
-                status: 200,
-                message: "Split added successfully"
-            }
-        } catch (err) {
-            return {
-                status: 500,
-                message: err
-            }
-        }
-    } else {
-        return {
-            status: 401,
-            message: "Invalid session"
-        }
-    }
-}
+		user.splits[name] = { name, schedule, split_workouts: workouts };
+
+		try {
+			await setUser(locals.username, JSON.stringify(user));
+			return {
+				status: 200,
+				message: 'Split added successfully'
+			};
+		} catch (err) {
+			return {
+				status: 500,
+				message: err
+			};
+		}
+	} else {
+		return {
+			status: 401,
+			message: 'Invalid session'
+		};
+	}
+};
