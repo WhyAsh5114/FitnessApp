f3fc09e875be5b88664a725742d7465322b8998e
WhyAsh5114 <whyash5114@gmail.com>
2022-07-19 18:57:13 +0530

    bug fix doesn't resolve event twice which would result in disturbed API endpoints

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,7 +3,6 @@ import { parse, serialize } from 'cookie';
 import { getUsernameFromSession } from './routes/api/_db';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const response = await resolve(event);
 	const cookies = parse(event.request.headers.get('cookie') || '');
 
 	if (cookies.session_id) {
@@ -15,18 +14,21 @@ export const handle: Handle = async ({ event, resolve }) => {
 			}
 		} catch (error) {
 			event.locals = {};
-			event.request.headers.set('set-cookie', serialize('session_id', '', {
-				path: '/',
-				httpOnly: true,
-				sameSite: 'strict',
-				maxAge: 0
-			}));
+			event.request.headers.set(
+				'set-cookie',
+				serialize('session_id', '', {
+					path: '/',
+					httpOnly: true,
+					sameSite: 'strict',
+					maxAge: 0
+				})
+			);
 			return resolve(event);
 		}
 	}
 
 	event.locals = {};
-	return response;
+	return resolve(event);
 };
 
 export const getSession: GetSession = (request) => {
