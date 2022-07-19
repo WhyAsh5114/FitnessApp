3f6abec951d11b871bb407fb05c7ae41d101840e
WhyAsh5114 <whyash5114@gmail.com>
2022-07-20 00:58:35 +0530

    disable parallelism as it causes conflicts within database

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,13 +22,13 @@ const config: PlaywrightTestConfig = {
 		timeout: 5000
 	},
 	/* Run tests in files in parallel */
-	fullyParallel: true,
+	fullyParallel: false,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
 	forbidOnly: !!process.env.CI,
 	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
 	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+	workers: 1,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: 'html',
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -41,7 +41,6 @@ const config: PlaywrightTestConfig = {
 		trace: 'on-first-retry',
 		launchOptions: {
 			slowMo: 100,
-			headless: false
 		}
 	},
 
